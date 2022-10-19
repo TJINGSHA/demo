c828af3ba7caf409dfab6f0bac6be01402749217
TLL <1820802236@qq.com>
2022-10-19 14:22:31 +0800

    初版

--- Gitck/src/main/java/com/example/service/EmpService.java
+++
@@ -1,5 +0,0 @@
-package com.example.service;
-
-public interface EmpService {
-    void sss();
-}
--- 坦克大战/src/com/up/tank/main/MainClass.java
+++
@@ -1,9 +0,0 @@
-package com.up.tank.main;
-
-import com.up.tank.view.MainView;
-
-public class MainClass {
-    public static void main(String[] args) {
-        new MainView();  // 创建一个界面
-    }
-}
